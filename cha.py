eb458717ca00f2b378c73ba93e0d2d8c64f5ad66
affan50pro <sweet.baby12325@gmail.com>
2024-12-12 20:16:59 +0400

    first commit

---
+++ cha.py
@@ -0,0 +1,8 @@
+num=int(input("enter your number : "))
+def cal(numb):
+    if numb%2==0:
+        return True
+    else:
+        return False
+ans=cal(num)
+print(ans)
---
+++ con5.py
@@ -0,0 +1,5 @@
+list=['a','b','c','d','e','f','g','h','i']
+for i in list:
+    if i=='e':
+        continue
+    print(i)
